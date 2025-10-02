4ccbdc866a71b708c69c893bb53ec45b31215c6b
MahmoudMohamedAli <mahmoudalielkout@gmail.com>
2025-10-02 14:41:54 +0200

    add condition var to wait on resources

--- objectPool_with_Multithreads/objpool_Multithreads.cpp
+++ objectPool_with_Multithreads/objpool_Multithreads.cpp
@@ -4,7 +4,20 @@
 #include<mutex>
 #include<chrono>
 #include<thread>
+#include<condition_variable>
+#include<sstream>
+#include<atomic>
 using namespace std;
+
+
+std::string get_thread_id() {
+  auto myid = std::this_thread::get_id();
+  std::stringstream ss;
+  ss << myid;
+  std::string mystr = ss.str();
+  return mystr;
+}
+
 
 class myclass
 {
@@ -21,7 +34,11 @@ private:
     vector<shared_ptr<myclass>> pool;
     int _size;
     mutex m1;
+   
+    condition_variable cndVar;
+
 public:
+ atomic<int> nextthreadindex ={0};
 objectpool(int size):_size(size)
 {
    for(int  i = 0;i < _size;i++ )
@@ -37,12 +54,13 @@ void releaseObject(shared_ptr<myclass> obj)
     unique_lock<mutex> lock(m1);
     if(_size > pool.size())
     {
-        cout<<"added myclass object back"<<endl;
+       // cout<<"added myclass object back"<<endl;
         pool.push_back(obj);
+        cndVar.notify_all();
     }
     else
     {
-        cout<<"can't add more objects" << endl;
+        //cout<<"can't add more objects" << endl;
     }
 
 }
@@ -50,22 +68,26 @@ void releaseObject(shared_ptr<myclass> obj)
 shared_ptr<myclass> acquireObject()
 {
     unique_lock<mutex> lock(m1);
-    if(pool.empty())
-    {
-        cout<<"pool is empty can't create  more objects........" << endl;
-        return nullptr;
-    }
-    else{
-        auto obj =  pool.front();
-        pool.erase(pool.begin());
-        cout<<"send object and delete it from pool"<<endl;
-        return obj;
-    }
+    cndVar.wait(lock,[this](){ return !pool.empty();});
+    auto obj =  pool.front();
+    pool.erase(pool.begin());
+  //  cout<<"send object and delete it from pool"<<endl;
+    return obj;
+    // if(pool.empty())
+    // {
+    //     cout<<"pool is empty can't create  more objects........" << endl;
+    //     return nullptr;
+    // }
+    // else{
+      
+    // }
 }
 };
 
 void worker(objectpool& pool)
 {
+
+    int next_index =  pool.nextthreadindex.fetch_add(1);
     for(int x=0; x < 15; x++)
     {
         auto obj = pool.acquireObject();
@@ -80,6 +102,7 @@ void worker(objectpool& pool)
         //null ptr
            printf("Thread  failed to acquire object\n");    
         }
+        cout << "thread Id: " << get_thread_id() <<" with indexed: " << next_index<<" num_ieration: "<< x << endl;
 
     }
 
@@ -89,10 +112,11 @@ void worker(objectpool& pool)
 
 int main()
 {
-objectpool pool(3); // Create a pool with a maximum size of 3
+  objectpool pool(1);
   std::vector<std::thread> workers; // Workers list will store all the threads.
-  int workers_count = 5; // How many threads you want ?
-  
+  int workers_count = 2; // How many threads you want ?
+
+
   for(int i=0; i<workers_count; i++) {
     workers.emplace_back(worker, std::ref(pool));
   }
